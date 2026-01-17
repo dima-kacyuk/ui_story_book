a9503d788aaf699090cb8460c153da0999bb7486
Evgeniy Kiryanow <kirjeka19977@gmail.com>
2026-01-17 15:01:28 +0200

    add setup storybook

---
+++ src/stories/Button.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./button.css";
+
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  primary?: boolean;
+  size?: "small" | "medium" | "large";
+  backgroundColor?: string;
+  label: string;
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  ...props
+}) => {
+  const mode = primary ? "storybook-button--primary" : "storybook-button--secondary";
+
+  return (
+    <button
+      type="button"
+      className={["storybook-button", `storybook-button--${size}`, mode].join(" ")}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
