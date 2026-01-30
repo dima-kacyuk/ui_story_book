3afacf7dca3ec1970be37f9734b5b909ebdae6fa
EvgeniyKiryanow <EvgeniyKiryanow@users.noreply.github.com>
2026-01-30 12:04:56 +0000

    deploy: 350a19a6cb7bee972ce1be4b6b0aedaa6bde4586

---
+++ assets/Header.stories-CTZDPw0O.js
@@ -0,0 +1,11 @@
+import{H as d}from"./Header-KDVO4Y-e.js";import"./jsx-runtime-D_zvdyIk.js";import"./Button-CkzL19Wp.js";const p={title:"Example/Header",component:d,tags:["autodocs"]},e={args:{user:{name:"Jane Doe"}}},r={args:{user:null}};var s,a,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: "Jane Doe"
+    }
+  }
+}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};var n,t,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    user: null
+  }
+}`,...(c=(t=r.parameters)==null?void 0:t.docs)==null?void 0:c.source}}};const i=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,i as __namedExportsOrder,p as default};
--- assets/preview-DwRFALPT.js
+++
@@ -1,1 +0,0 @@
-const o={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}}},tags:["autodocs"]};export{o as default};
