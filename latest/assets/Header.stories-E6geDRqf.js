fe69e76ffa080fa4f0de750d378a5b895229fd1f
Evgeniy Kiryanow <kirjeka19977@gmail.com>
2026-02-01 13:14:36 +0200

    Updates

--- latest/assets/Header.stories-E6geDRqf.js
+++
@@ -1,9 +0,0 @@
-import{H as i}from"./Header-DGN7Q5yG.js";import"./index-CtOEgLBf.js";import"./jsx-runtime-D_zvdyIk.js";import"./bundle-mjs-BNe0Xlio.js";import"./createLucideIcon-CFZQIwQc.js";import"./chevron-down-B0yM7G96.js";import"./x-D_mDA-0y.js";import"./_commonjsHelpers-CqkleIqs.js";const w={title:"Layout/djHeader",component:i,parameters:{layout:"fullscreen"},tags:["autodocs"]},e={args:{}},r={parameters:{viewport:{defaultViewport:"mobile1"}}};var o,t,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {}
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};var s,p,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  parameters: {
-    viewport: {
-      defaultViewport: "mobile1"
-    }
-  }
-}`,...(m=(p=r.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const H=["Desktop","Mobile"];export{e as Desktop,r as Mobile,H as __namedExportsOrder,w as default};
